00ea104224df5bc9928adff378b7fbb34fb13836
zeljkolivaja <zeljaos@gmail.com>
2023-10-06 18:25:12 +0200

    Created responsive layot by using chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      // define responsive templates for mobile and desktop
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">Nav</GridItem>
+      {/* render only on large screens or above */}
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
