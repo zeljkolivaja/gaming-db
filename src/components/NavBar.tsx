34682ba7963f28dfad5ea4921df6d5528a384159
zeljkolivaja <zeljaos@gmail.com>
2023-10-16 21:13:46 +0200

    Implemented color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image} from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
   return (
     // HSstack (horizontal stack) is component for laying out items horizontaly
-    <HStack>
-      {/* chakra image component, location of image is imported just like regular component */}
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize={40}></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch/>
     </HStack>
   );
 }
