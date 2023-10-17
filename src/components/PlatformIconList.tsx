985185aa0f9847c65edf22e59be3a08125f73f46
zeljkolivaja <zeljaos@gmail.com>
2023-10-17 18:15:21 +0200

    added platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {/* platform is destructured, so we dont have to write platform.platform.name...*/}
+      {platforms.map((platform) => {
+        return <Icon as={iconMap[platform.slug]} color="gray.400" />;
+      })}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,10 +6,19 @@ interface FetchGamesResponse {
   results: Game[];
 }
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  //   due to api design, parent platforms is array of objects, where
+  //   each object has platform property of type Platform
+  parent_platforms: { platform: Platform }[];
 }
 
 function useGames() {
