1feb0813aca076a2f06f2236b8481a32ba262185
zeljkolivaja <zeljaos@gmail.com>
2023-10-18 12:06:18 +0200

    Created component for loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,26 +25,31 @@ export interface Game {
 function useGames() {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     //FetchGameResponse provide shape of response object (interface)
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+        setLoading(false);
+      })
       .catch((error) => {
         if (error.name === "CanceledError") {
           return;
         }
-
         setError(error.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGames;
